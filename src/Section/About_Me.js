b6674f3eb1629ecde7eeeca0e26d0fc69d069b96
Mickael <60462022+MickaelMedia@users.noreply.github.com>
2021-03-08 13:30:41 -0500

    title work

--- src/Section/About_Me.js
+++ src/Section/About_Me.js
@@ -8,10 +8,11 @@ function AboutMe() {
                 <p>I am a guy who just really wants to spend his time doing 3 things. Discussing or working in health care; going out for a really epic ride one my mountian bike and finally coding new or just fun stuff.</p>
                 <p>I choose to get into Health because of the different possibilities as a bed-side nurse, assistant, teacher, chief nurse and etc.... But, I still have fun learning how to code better and also how to ride my bike with STEEEEEEEEEEZE xD</p>
                 <p>If you want to contact me to give me suggestions for my website or to talk about something cool in nursing or MTB hit me up</p>
-                <p>mickaelmediawebsite@gmail.com</p>
+                <p>mickaelthebaud@gmail.com</p>
             </article>
             <article>
                 <h2>Dev Notes</h2>
+                <p>2021-03-08: Redid my titles so they pop out more from the background. Got inspired from Mahalo My Dude but I added text shadow </p>
                 <p>2021-03-07: Touching up About Me and Welcome Sections for grammar</p>
                 <p>2021-03-05: Just retouched the footer to make it look cleaner</p>
                 <p>2021-03-04: Going to start a DevNotes just to write down my changes. Might just be a copy paste from my github repo.</p>
